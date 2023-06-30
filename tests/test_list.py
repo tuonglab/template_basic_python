dc6ed10625c00759d231cc07507073b8511928af
Zewen Kelvin Tuong <z.tuong@uq.edu.au>
2023-06-30 12:09:13 +1000

    Update test_list.py

--- tests/test_list.py
+++ tests/test_list.py
@@ -36,10 +36,10 @@ def test_unique_items():
 
 def test_unique_list_format():
     assert learn_list.unique_list_format() == [
-        "banana",
+        "apple",
+        "leek",
         "boy",
         "cheese",
-        "leek",
-        "apple",
+        "banana",
         "pie",
     ]
